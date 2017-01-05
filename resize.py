e7fd0d954773bced372b47e8dec7e8af9322788b
MrSimonC <iamontheinternet@yahoo.com>
2017-01-05 15:33:22 +0000

    v1.1: won't resize a picture up which loses quality

--- resize.py
+++ resize.py
@@ -1,14 +1,17 @@
 from PIL import Image
 import os
 import sys
-__version__ = '1.0'
+__version__ = '1.1'
+# v1.1: won't resize a picture up which loses quality
 
 
 def resize(file_path, new_width, sub_folder='resized'):
     # Resize any input image, outputting to a folder
     img = Image.open(file_path)
+    width, height = img.size
     new_height = int((float(img.size[1]) * float((new_width / float(img.size[0])))))
-    img = img.resize((new_width, new_height), Image.ANTIALIAS)
+    if int(new_width) < width:  # always resize down, don't resize up
+        img = img.resize((new_width, new_height), Image.ANTIALIAS)
     source_folder, source_file = os.path.split(file_path)
     save_to_dir = os.path.join(source_folder, sub_folder)
     if not os.path.exists(save_to_dir):
